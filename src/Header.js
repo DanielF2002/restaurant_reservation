2f7da18ca66b6a53956c646cf7261da565799075
Xin Feng <126309503+DanielF2002@users.noreply.github.com>
2023-09-22 12:04:15 +0300

    do some start job

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import { ReactComponent as HeaderLogo } from './assets/Logo.svg';
+
+export default function Header(){
+    return (
+        <header>
+            <HeaderLogo title="little lemon logo"/>
+        </header>
+    )
+}
---
+++ src/Main.js
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <main>
+            <article>
+                <p>Main</p>
+            </article>
+        </main>
+    )
+}
